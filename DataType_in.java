1494efa954e83ca5fe16f62216063b2b557cd3ac
Kunal Patil <kunalpatil29th@gmail.com>
2026-06-07 13:32:33 +0530

    add

---
+++ DataType_in.java
@@ -0,0 +1,29 @@
+
+
+public class DataType_in {
+    public static void main(String[] args) {
+        // Sum of a and b
+        int a = 10;
+        int b = 20;
+        int sum = (a + b)/3;
+        System.out.print(sum);
+        
+
+
+
+        // Comments in java
+        // 1. Single line comments
+        //  This is commets
+
+        // 2. Multline line comment
+        // 2 . Multiline comments
+        /*
+
+        This is commets
+        
+        */
+    
+
+    }
+    
+}
--- JavaBasic.java
+++ JavaBasic.java
@@ -1,6 +1,8 @@
 public class JavaBasic {
 
     public static void main(String[] args) {
-        System.out.println("Hello Wolrld");
+        System.out.println("1 2 3 4 5");
+        System.out.println("HI");
+        System.out.print("Hello world\n"); 
    }
 }

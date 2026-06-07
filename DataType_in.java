3dbd9ee77cab57c75281abf68775386162499998
Kunal Patil <kunalpatil29th@gmail.com>
2026-06-07 14:00:35 +0530

    add

--- DataType_in.java
+++ DataType_in.java
@@ -1,27 +1,29 @@
-
-
+import java.util.Scanner;
 public class DataType_in {
     public static void main(String[] args) {
-        // Sum of a and b
-        int a = 10;
-        int b = 20;
-        int sum = (a + b)/3;
-        System.out.print(sum);
+
+
+        // Sum of two products in user input 
+        Scanner scanner = new Scanner(System.in);
+       System.out.println("Enter a number a = ");
+       int a = scanner.nextInt();
+
+       System.out.println("Enter a number b = ");
+       int b = scanner.nextInt();
+
+       int sum = a + b ;
+       System.out.print(sum);
+
+
+
+
+       
         
 
 
 
-        // Comments in java
-        // 1. Single line comments
-        //  This is commets
 
-        // 2. Multline line comment
-        // 2 . Multiline comments
-        /*
 
-        This is commets
-        
-        */
     
 
     }

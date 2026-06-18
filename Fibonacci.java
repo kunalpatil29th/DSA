d6ff1b9d967dc181249f64e84597a1694299772a
Kunal Patil <kunalpatil29th@gmail.com>
2026-06-18 21:16:13 +0530

    Add fibonacci example

---
+++ Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static void main(String[] args) {
+        int terms = 8;
+        int first = 0;
+        int second = 1;
+
+        for (int i = 0; i < terms; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+    }
+}

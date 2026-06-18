0d6f824cbe8bcf83d018a01e2e87398a70eeaf92
Kunal Patil <kunalpatil29th@gmail.com>
2026-06-18 21:16:11 +0530

    Add bubble sort example

---
+++ BubbleSort.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] numbers = {9, 4, 7, 1, 3};
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - i - 1; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
